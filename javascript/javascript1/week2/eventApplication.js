32a1761806032dfc0cf4e5d734566bf26a9e2139
Mahie <mahie.akhtar@outlook.com>
2019-09-15 10:05:42 +0200

    after code review

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -1,13 +1,5 @@
-
 function getDayofWeek(day) {
-    const weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    const weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
     return weekday[day];
 }
 
