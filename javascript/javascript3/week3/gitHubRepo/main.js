e9e8b8659e81a1eb95e83f49e29f78a539ae59c7
Mahie <mahie.akhtar@outlook.com>
2019-11-16 06:44:03 +0100

    Removed promiseAll after code review

--- javascript/javascript3/week3/gitHubRepo/main.js
+++ javascript/javascript3/week3/gitHubRepo/main.js
@@ -1,14 +1,9 @@
 let ul = document.querySelector('ul');
-const user1 = fetch('https://api.github.com/search/repositories?q=user:Nazaqat');
-const user2 = fetch('https://api.github.com/search/repositories?q=user:HouriaDanesh');
-const user3 = fetch('https://api.github.com/search/repositories?q=user:Sreelatha-Vallabhaneni');
+const user1 = fetch('https://api.github.com/search/repositories?q=user:Nazaqat').then(response => response.json());
+const user2 = fetch('https://api.github.com/search/repositories?q=user:HouriaDanesh').then(response => response.json());
+const user3 = fetch('https://api.github.com/search/repositories?q=user:Sreelatha-Vallabhaneni').then(response => response.json());
 
 Promise.all([user1, user2, user3])
-    .then(responses => {
-        const jsonResponses = responses.map(response => response.json());
-        console.log(jsonResponses);
-        return Promise.all(jsonResponses);
-    })
     .then(responses => {
         console.log(responses);
         responses.map((user) => {
@@ -27,3 +22,18 @@ Promise.all([user1, user2, user3])
         })
     })
 
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+    for (var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+        // Add it to the list:
+        list.appendChild(item);
+    }
+    // Finally, return the constructed list:
+    return list;
+}
