9ed54d03f7705e6862de9ba701e7b32bb1c3a4f3
Mahie <mahie.akhtar@outlook.com>
2019-12-04 23:20:13 +0100

    Changes after review

--- databases/week3/additionalQueries.sql
+++ databases/week3/additionalQueries.sql
@@ -3,29 +3,28 @@
 ---- Meal
 INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date) 
 VALUES('pasta', 'pesto with pasta', 'copenhagen', '2019-12-03', 4, 300.15, now());
-INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date)   
+INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date) 
 VALUES('fried rice', 'chinese fried rice with chicken', 'aarhus', '2019-12-04', 2, 100.15, now());
-INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date)   
+INSERT INTO meal (title,description,location,_when,max_reservations,price,created_date)  
 VALUES('sandwich', 'vegetable sandwich', 'norreport', '2019-12-07', 2, 200, now());
-INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date)   
+INSERT INTO meal (title,description,location,_when,max_reservations,price,created_date)
 VALUES('salat', 'chicken salat', 'copenhagen', '2019-12-05', 4, 50.30, now());
-INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date)
+INSERT INTO meal (title,description,location,_when,max_reservations,price,created_date)  
 VALUES('roll', 'chicken roll with vegetable', 'kongens nytorv', '2019-12-31', 4, 200.15, now());
-INSERT INTO meal(title,description,location,_when,max_reservations,price,created_date)
+INSERT INTO meal (title,description,location,_when,max_reservations,price,created_date)
 VALUES('pittabread', 'pittabread with chicken', 'copenhagen', '2019-12-15', 4, 245.15, now());
 --- Reservation
-INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(3,3,now());
-INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(2,4,now());
-INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(4,8,now());
-INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(6,10,now());
-INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(1,5,now());
+INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(3, 3, now());  
+INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(2, 4, now());
+INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(4, 8, now());   
+INSERT INTO reservation(number_of_guests,meal_id,created_date) VALUES(6, 10, now());   
+INSERT INTO reservation (number_of_guests,meal_id,created_date) VALUES(1, 5, now());  
 --- Review
-INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('very good','very delecious meal',3,4,now());
-INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('good','tasty meal',1,4,now());
-INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('not good','yummy',8,4,now());
-INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('good','delecious meal',1,4,now());
-INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('good','delecious meal',5,5,now());
-
+INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('very good', 'very delecious meal', 3, 4, now());   
+INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('good', 'tasty meal', 1, 4, now());    
+INSERT INTO review(title,description,meal_id,stars,created_date) VALUES('not good', 'yummy', 8, 4, now());   
+INSERT INTO review (title,description,meal_id,stars,created_date) VALUES('good', 'delecious meal', 1, 4, now());
+INSERT INTO review (title,description,meal_id,stars,created_date) VALUES('good', 'delecious meal', 5, 5, now());
 -- Get meals that has a price smaller than a specific price fx 90
 SELECT * FROM meal where price < 100;
 -- Get meals that still has available reservations
@@ -35,12 +34,14 @@ SELECT * FROM meal WHERE title like'%pa%';
 -- Get meals that has been created between two dates
 SELECT * FROM meal WHERE created_date BETWEEN '2019-11-20' AND '2019-11-26';
 -- Get only specific number of meals fx return only 5 meals
-SELECT * FROM meal limit 5; 
+SELECT * FROM meal limit 5;
 -- Get the meals that have good reviews
-SELECT * FROM meal JOIN review ON meal.id = review.meal_id WHERE review.title = 'good' OR review.title = 'very good';
+SELECT * FROM meal JOIN review ON meal.id = review.meal_id WHERE review.stars = 'good' OR review.stars = 'very good';
 -- Get reservations for a specific meal sorted by created_date
 SELECT * FROM meal JOIN reservation ON meal.id = reservation.meal_id WHERE reservation.created_date;
 -- Sort all meals by average number of stars in the reviews
-SELECT meal.title, AVG(review.stars) AS average_no_of_stars FROM meal JOIN review ON meal.id = review.meal_id
-GROUP BY meal.title ORDER BY average_no_of_stars;	 
+SELECT meal.title, AVG(review.stars) AS average_no_of_stars
+FROM meal JOIN review ON meal.id = review.meal_id
+GROUP BY meal.id
+ORDER BY average_no_of_stars;	 
     
--- databases/week3/tables.sql
+++ databases/week3/tables.sql
@@ -3,37 +3,41 @@ CREATE DATABASE meal_sharing_db;
 USE meal_sharing_db;
 
 -- Meal table
-CREATE TABLE meal(
+CREATE TABLE meal
+(
 id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-title VARCHAR(255) NOT NULL,
+title VARCHAR (255) NOT NULL,
 description TEXT NULL DEFAULT NULL,
-location VARCHAR(255) NOT NULL,   
-_when DATETIME NOT NULL,
-max_reservations INT(10),
+location VARCHAR  (255) NOT NULL,    
+`when` DATETIME NOT NULL,
+max_reservations INT(10),  
 price DECIMAL(5,2),
 created_date DATETIME NOT NULL,
-PRIMARY KEY(id)
+PRIMARY KEY (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Reservation table
-CREATE TABLE reservation(
-id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,      
-number_of_guests INT(10) NOT NULL,       
-meal_id INT(10) UNSIGNED NOT NULL,       
+    -- Reservation table
+CREATE TABLE reservation
+    (
+id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,         
+number_of_guests INT(10) NOT NULL,              
+meal_id INT(10) UNSIGNED NOT NULL,           
 created_date DATETIME NOT NULL,
-PRIMARY KEY (id),      
-CONSTRAINT fk_meal_id FOREIGN KEY(meal_id) REFERENCES meal(id) ON DELETE CASCADE        
+PRIMARY KEY(id),             
+CONSTRAINT fk_meal_id FOREIGN KEY(meal_id) REFERENCES meal (id) ON  DELETE CASCADE              
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-        
--- Review table
-CREATE TABLE review(
-id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,           
-title VARCHAR (255) NOT NULL,            
+       
+   -- Review table
+CREATE TABLE review (  
+id INT(10)  UNSIGNED NOT NULL AUTO_INCREMENT,                    
+title VARCHAR (255) NOT NULL,                       
 description TEXT NOT NULL,
-meal_id INT(10) UNSIGNED NOT NULL,          
-stars INT(10) NOT NULL,           
+meal_id INT(10) UNSIGNED NOT NULL,                      
+stars INT(10) NOT NULL,                       
 created_date DATETIME NOT NULL,
-PRIMARY KEY(id),          
-CONSTRAINT fk_meal_id_review FOREIGN KEY(meal_id) REFERENCES reservation(meal_id) ON DELETE CASCADE           
+PRIMARY KEY(id),                     
+CONSTRAINT fk_meal_id_review FOREIGN KEY(meal_id) REFERENCES reservation(meal_id) ON DELETE CASCADE                               
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-            
+-- change column name on meal table          
+ALTER TABLE meal
+CHANGE COLUMN _when `when` DATETIME NOT NULL;
